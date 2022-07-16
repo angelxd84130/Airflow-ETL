bc510a002aff496609aae264f774b51b3254228e
ANGEL <angelxd84130@gmail.com>
2022-07-16 22:24:14 +0800

    fixed real-tiem loading

--- bigquery_eventtracking_regular_report_module/storage.py
+++ bigquery_eventtracking_regular_report_module/storage.py
@@ -7,39 +7,73 @@ import logging
 
 def db_connect():
     logging.info('connecting to local db..')
-    conn = pymysql.connect(host="127.0.0.1", user="root", password='zxcv1234', port=3306, db='BetSlip')
+    conn = pymysql.connect(host="127.0.0.1", user="root", password='', port=)
     cur = conn.cursor()
-    
+    logging.info('connecting successful.')
     return cur, conn
 
-def save_transaction_error(df):
-    cur, conn = db_connect()
-    logging.info('connecting successful. writting transaction_error in db..')
-    for rows in df.itertuples():
-        rows = list(tuple(rows)[1:])
-        rows = [str(i) for i in rows]
-        sql = '''INSERT INTO EventTracking.transaction_error_count (event_date, error_code, sum_of_error, error_rate)
-            VALUES ("%s", "%s", "%s", "%s");
-            ''' % tuple(rows)
 
+
+def save_transaction_error(df, eventTime):
+    cur, conn = db_connect()
+    table = 'EventTracking.transaction_error_count'
+
+    logging.info('processing transaction_error in db..')
+    for index, row in df.iterrows():
+        error_code = str(row['error_code'])
+        sum_of_error = row['sum_of_error']
+        error_rate = row['error_rate']
+
+        logging.info('searching exist in db..' + error_code)
+        sql = f"""SELECT * From {table} WHERE event_date="{eventTime}" and error_code="{error_code}"; """
+        cur.execute(sql)
+
+        if(cur.fetchone()):
+            logging.info('updating data..')
+            sql = f""" 
+                    UPDATE {table} SET sum_of_error = {sum_of_error}, error_rate = {error_rate} 
+                    WHERE event_date = "{eventTime}" and error_code = "{error_code}";
+                  """ 
+        else:
+            logging.info('inserting data..')
+            sql = f"""
+                    INSERT INTO {table} (event_date, error_code, sum_of_error, error_rate)
+                    VALUES ("{eventTime}", "{error_code}", {sum_of_error}, {error_rate});
+                """ 
         cur.execute(sql)
         conn.commit()
-
-    logging.info('writting successfully!')
+    logging.info('successfully!')
     
 
-def save_transaction_success_rate(df):
+
+def save_transaction_rate(df, eventTime):
     cur, conn = db_connect()
-    logging.info('connecting successful. writting transaction_success_rate in db..')
+    table = 'EventTracking.transaction_success_rate'
 
-    for rows in df.itertuples():
-        rows = list(tuple(rows)[1:])
-        rows = [str(i) for i in rows]
-        sql = '''INSERT INTO EventTracking.transaction_success_rate (event_date, sum_of_success, sum_of_fail, fail_rate)
-            VALUES ("%s", "%s", "%s", "%s");
-            ''' % tuple(rows)
+    logging.info('processing transaction_rate in db..')
+    for index, row in df.iterrows():
+        sum_of_success = row['sum_of_success']
+        sum_of_fail = row['sum_of_fail']
+        success_rate = row['success_rate']
 
+        logging.info('searching exist in db..' + eventTime)
+        sql = f"""SELECT * From {table} WHERE event_date="{eventTime}"; """
+        cur.execute(sql) 
+
+        if(cur.fetchone()):
+            logging.info('updating data..')
+            sql = f"""
+                    UPDATE {table} SET sum_of_success={sum_of_success}, sum_of_fail={sum_of_fail}, success_rate={success_rate}            
+                    WHERE event_date = "{eventTime}";
+                """
+        else:
+            logging.info('inserting data..')
+        
+            sql = f"""
+                    INSERT INTO {table} (event_date, sum_of_success, sum_of_fail, success_rate)
+                    VALUES ("{eventTime}", {sum_of_success}, {sum_of_fail}, {success_rate});
+                """ 
         cur.execute(sql)
         conn.commit()
-
-    logging.info('writting successfully!')
+        
+    logging.info('successfully!')
