63881bae0a9da76137b4cbf598041b69f735aad5
ANGEL <angelxd84130@gmail.com>
2022-07-16 22:19:12 +0800

    fixed sql to simplify data loading

--- bigquery_eventtracking_regular_report_module/_query.py
+++ bigquery_eventtracking_regular_report_module/_query.py
@@ -7,25 +7,48 @@ def _setup_credential():
     credential_path = "/home/albert/airflow/dags/bigquery_eventtracking_regular_report_module/sg-prod-readonly-303206-cb8365379fd6.json"
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
-
 _setup_credential()
 _client = bigquery.Client()
 
 
 
-def get_eventTracking_data_by_date(startTime: str = None, endTime: str = None) -> 'dataframe':
-    if startTime is None or endTime is None:
-        raise ValueError("time parameter must be specified, ")
+''' daily '''
 
-    query = f"""
-        SELECT * FROM `sg-prod-303206.prod.sportsUserBehaviour` 
-        WHERE DATE(createTime)>="{startTime}" and DATE(createTime)<"{endTime}"
-        and eventId in (200005, 200006, 200007, 200008, 200009)
+def get_daily_transaction_rate(startTime: str=None, endTime: str=None) -> 'dataframe':
+    if startTime is None:
+        raise ValueError("time parameter must be specified. ")
 
-    """
+    query = """
+        SELECT sum(if(actionValue5='true', 1, 0)) as sum_of_success, 
+                sum(if(actionValue5='false', 1, 0)) as sum_of_fail
+                 FROM `sg-prod-303206.prod.sportsUserBehaviour` 
+        WHERE createTime between "%s" and "%s"
+        and eventId in (200005, 200006, 200007, 200008, 200009, 200080, 200081, 200082)
+
+    """ % (startTime, endTime)
 
     query_info = _client.query(query)
     query_info = query_info.to_dataframe()
     return query_info
 
 
+def get_daily_transaction_error(startTime: str=None, endTime: str=None) -> 'dataframe':
+    if startTime is None:
+        raise ValueError("time parameter must be specified. ")
+
+    query = """
+        SELECT actionValue6 as error_code, count(actionValue6) as sum_of_error 
+        FROM `sg-prod-303206.prod.sportsUserBehaviour`  
+        WHERE createTime between "%s" and "%s" 
+        and eventId in (200005, 200006, 200007, 200008, 200009, 200080, 200081, 200082) and actionValue5='false' 
+        group by error_code 
+
+    """ % (startTime, endTime)
+
+    query_info = _client.query(query)
+    query_info = query_info.to_dataframe()
+    return query_info
+
+
+
+''' weekly '''
