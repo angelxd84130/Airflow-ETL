c189fe3c1d5037825bc91544bd97a02aa8f0c44b
ANGEL <angelxd84130@gmail.com>
2022-07-31 21:00:57 +0800

    Update bigquery_eventtracking_regular_report.py

--- bigquery_eventtracking_regular_report.py
+++ bigquery_eventtracking_regular_report.py
@@ -7,25 +7,37 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 
+
 def get_bigquery_eventtracking():
 
+    # convert time zone from utc+0 to utc+8
+    '''
+    # for loading current data
+    eventTime =  datetime.now().strftime('%Y-%m-%d')
+    startTime = (datetime.now() + timedelta(days=-1)).strftime('%Y-%m-%d') + 'T16:00:00'
+    endTime = datetime.now().strftime('%Y-%m-%d') + 'T16:00:00'
+    
+    '''
     # for loading past data
-    for n in range(0,1):
-        
-        # convert time zone from utc+0 to utc+8
+    for n in range(0,2):
         eventTime = (datetime.now() + timedelta(days=-n)).strftime('%Y-%m-%d')
         startTime = (datetime.now() + timedelta(days=-n-1)).strftime('%Y-%m-%d') + 'T16:00:00'
         endTime = (datetime.now() + timedelta(days=-n)).strftime('%Y-%m-%d') + 'T16:00:00'
+
         logging.info('loading data by date:' + eventTime)
 
         # download data (Extrat+Transform)
         transaction_rate = api.get_daily_transaction_rate(startTime, endTime, eventTime)
         transaction_error = api.get_daily_transaction_error(startTime, endTime, eventTime)
+        sport_transaction_result = api.get_daily_sport_transaction(startTime, endTime, eventTime)
+        sport_transaction_error = api.get_daily_sport_error(startTime, endTime, eventTime)
 
 
         # storage data (Load)
         storage.save_transaction_rate(transaction_rate, eventTime)
         storage.save_transaction_error(transaction_error, eventTime)
+        storage.save_daily_sport_transaction(sport_transaction_result, eventTime)
+        storage.save_daily_sport_error(sport_transaction_error, eventTime)
         time.sleep(0.5)
 
 
