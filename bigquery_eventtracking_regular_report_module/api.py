17e2f789120d34c6e0bb1e2fe92454d3a5f76f60
ANGEL <angelxd84130@gmail.com>
2022-07-31 21:03:39 +0800

    Update api.py

--- bigquery_eventtracking_regular_report_module/api.py
+++ bigquery_eventtracking_regular_report_module/api.py
@@ -3,16 +3,28 @@ from bigquery_eventtracking_regular_report_module import parse
 
 ''' daily '''
 
-def get_daily_transaction_rate(startTime, endTime, eventTime):
+def get_daily_transaction_rate(startTime, endTime, eventTime) -> 'dataframe':
     df = _query.get_daily_transaction_rate(startTime, endTime)
     df = parse.mapping_transaction_rate(df ,eventTime)
     print(df)
     return df
 
-def get_daily_transaction_error(startTime, endTime, eventTime):
+def get_daily_transaction_error(startTime, endTime, eventTime) -> 'dataframe':
     df = _query.get_daily_transaction_error(startTime, endTime)
     df = parse.mapping_transaction_error(df, eventTime)
     print(df)
     return df
 
 
+def get_daily_sport_transaction(startTime, endTime, eventTime) -> 'dataframe':
+    df = _query.get_daily_sport_transaction(startTime, endTime)
+    df = parse.mapping_sport_transaction(df, eventTime)
+    print(df)
+    return df
+
+
+def get_daily_sport_error(startTime, endTime, eventTime) -> 'dataframe':
+    df = _query.get_daily_sport_error(startTime, endTime)
+    df = parse.mapping_sport_error(df, eventTime)
+    print(df)
+    return df
